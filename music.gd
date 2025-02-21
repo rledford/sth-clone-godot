d05b9aabb3d06a8d1e7c9cd833e3faf6bd87aa6e
Santiago Kent <santiagokent@gmail.com>
2025-02-21 10:55:17 -0300

    feat: integrate music module with waves

--- music.gd
+++ music.gd
@@ -15,47 +15,55 @@ const tracks = [
 	STANDARD_ENCOUNTER_THEME
 ]
 
-var volume: float = -10
-var initial_volume: float = volume - 40
-var fade_in_duration: float = 6
-var fade_out_duration: float = 2
+var action_volume: int = -10
+var menu_volume: int = -20
+var silent_volume: int = -50
 
 var _last_played_track: Resource
 
 func _ready() -> void:
-	_play_random_track()
+	self.volume_db = silent_volume
+
 	SignalBus.game_over.connect(func(_wave: int): _on_game_over())
+	SignalBus.wave_started.connect(func(_wave: int): _on_wave_start())
+	SignalBus.break_started.connect(func(_break_time: float): _on_break_start())
 
 func _on_game_over() -> void:
-	_play_random_track()
+	_play_random_track(menu_volume)
 
-func _play_random_track() -> void:
-	_play(_get_random_track())
+func _on_wave_start() -> void:
+	_play_random_track(action_volume)
 
-func _play(res: Resource) -> void:
+func _on_break_start() -> void:
+	await _fade_out(silent_volume, 3)
+	self.stop()
+
+func _play_random_track(volume: int) -> void:
+	_play(_get_random_track(), volume)
+
+func _play(res: Resource, volume: int) -> void:
 	if self.playing:
-		await _fade_out()
+		await _fade_out(silent_volume, 2)
 		self.stop()
 		
 	self.set_stream(res)
-	self.volume_db = initial_volume
 	self.play()
 	_last_played_track = res
-	await _fade_in()
+	await _fade_in(volume, 4)
 
 # These two look quite similar, but they'll probably need to be tweaked separately
-func _fade_out() -> Signal:
+func _fade_out(volume: int, duration: int) -> Signal:
 	var fade_out = create_tween()
 	fade_out.set_ease(Tween.EaseType.EASE_OUT)
 	fade_out.set_trans(Tween.TransitionType.TRANS_QUAD)
-	fade_out.tween_property(self, "volume_db", initial_volume, fade_out_duration)
+	fade_out.tween_property(self, "volume_db", volume, duration)
 	return fade_out.finished
 
-func _fade_in() -> Signal:
+func _fade_in(volume: int, duration: int) -> Signal:
 	var fade_in = create_tween()
 	fade_in.set_ease(Tween.EaseType.EASE_OUT)
 	fade_in.set_trans(Tween.TransitionType.TRANS_QUAD)
-	fade_in.tween_property(self, "volume_db", volume, fade_in_duration)
+	fade_in.tween_property(self, "volume_db", volume, duration)
 	return fade_in.finished
 
 func _get_random_track() -> Resource:
