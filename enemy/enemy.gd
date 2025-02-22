d4aff20353eb7cc4ee66127195d49bb398d45b22
Santiago Kent <santiagokent@gmail.com>
2025-02-22 13:49:02 -0300

    feat: new custom_timer component, reworked the waves and spawning logic to make use of timers, add a new died signal to enemies to make it snappier

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,7 +1,8 @@
-extends CharacterBody2D
 class_name Enemy
+extends CharacterBody2D
 
 signal hit(damage: float)
+signal died
 
 @export var coin_reward: int = 1
 @export var hp: int = 2
@@ -13,36 +14,45 @@ var hit_particle = preload("res://particles/enemy_hit_particle.tscn")
 var hit_particle_color: Color = Color.RED
 var fsm: StateMachine
 
+
 func filter_attack_areas(area) -> bool:
 	return area is CollisionShape2D
 
+
 func _update_attack_position() -> void:
-	var areas = get_tree().get_nodes_in_group("attack_areas").filter(filter_attack_areas) as Array[CollisionShape2D]
+	var areas = (
+		get_tree().get_nodes_in_group("attack_areas").filter(filter_attack_areas)
+		as Array[CollisionShape2D]
+	)
 	var target = areas.pick_random() as CollisionShape2D
 	if not target:
 		attack_position = Vector2.INF
-		return 
+		return
 	var target_size = target.shape.get_rect().size as Vector2
 	var rand_x = target.global_position.x - (target_size.x * 0.5)
-	
+
 	var rand_y = randi_range(
 		target.global_position.y - (target_size.y * 0.5),
 		target.global_position.y + (target_size.y * 0.5)
 	)
-	
+
 	attack_position = Vector2(rand_x, rand_y)
-	
+
+
 func has_valid_attack_position() -> bool:
 	return attack_position and attack_position != Vector2.INF
 
+
 func attack_anim_finished() -> void:
 	SignalBus.player_hit.emit(damage)
+
 
 func flip() -> void:
 	if velocity.x > 0.0:
 		scale.x = scale.y * 1
 	elif velocity.x < -0.0:
 		scale.x = scale.y * -1
+
 
 func _handle_hit(_amount: int) -> void:
 	hp -= _amount
@@ -54,11 +64,14 @@ func _handle_hit(_amount: int) -> void:
 	particle.one_shot = true
 	add_child(particle)
 
+
 func _enter_tree() -> void:
 	hit.connect(_handle_hit)
-	
+
+
 func _on_area_2d_area_entered(_area: Area2D) -> void:
 	fsm.change_state("attack")
+
 
 func _on_area_2d_area_exited(_area: Area2D) -> void:
 	fsm.change_state("idle")
--- enemy/enemy_spawn.gd
+++ enemy/enemy_spawn.gd
@@ -1,5 +1,7 @@
-extends Node2D
 class_name EnemySpawn
+extends Node2D
+
+signal wave_cleared
 
 const FlyingEye = preload("res://enemy/flying_eye/flying_eye.tscn")
 const Goblin = preload("res://enemy/goblin/goblin.tscn")
@@ -7,56 +9,39 @@ const Skeleton = preload("res://enemy/skeleton/skeleton.tscn")
 const Mushroom = preload("res://enemy/mushroom/mushroom.tscn")
 
 # Dictionary that maps diffuclty score to a set of enemies
-const enemies: Dictionary = {
-	1: [FlyingEye],
-	2: [Goblin],
-	4: [Skeleton],
-	6: [Mushroom]
-}
+const ENEMIES: Dictionary = {1: [FlyingEye], 2: [Goblin], 4: [Skeleton], 6: [Mushroom]}
 
-signal spawned_all_enemies
-
-var _wave: Array = []
-var _spawn_timer: float = 0.0
 
 func spawn_wave(wave: Array) -> void:
-	_wave = wave
+	var last_spawned_enemy
+
+	for item in wave:
+		last_spawned_enemy = _spawn(item.score)
+		await get_tree().create_timer(item.spawn_timer).timeout
+
+	last_spawned_enemy.died.connect(wave_cleared.emit)
+
 
 func is_valid_score(score: int) -> bool:
-	return enemies.has(score)
+	return ENEMIES.has(score)
 
-func _physics_process(delta: float) -> void:
-	if _wave.is_empty():
-		return
 
-	_spawn_timer = max(_spawn_timer - delta, 0)
-	
-	if _spawn_timer > 0:
-		return
-
-	var current = _wave.pop_front()
-	_spawn(current.score, current.spawn_timer)
-
-	if _wave.is_empty():
-		print("Spawned all enemies!")
-		spawned_all_enemies.emit()
-
-func _spawn(score: int, timer: float) -> void:
-	var enemy = enemies.get(score).pick_random().instantiate()
+func _spawn(score: int) -> Enemy:
+	var enemy = ENEMIES.get(score).pick_random().instantiate()
 	enemy.add_to_group("enemies")
 	enemy.global_position = _random_spawn_position()
 	add_child(enemy)
-	_spawn_timer = timer
-	
+	return enemy
+
+
 func _random_spawn_position() -> Vector2:
 	var areas = get_tree().get_nodes_in_group("enemy_spawn_areas") as Array[CollisionShape2D]
 	var area: CollisionShape2D = areas.pick_random()
 	var size: Vector2 = area.shape.get_rect().size
-	
+
 	var rand_x = area.global_position.x
 	var rand_y = randf_range(
-		area.global_position.y - (size.y * 0.5),
-		area.global_position.y + (size.y * 0.5)
+		area.global_position.y - (size.y * 0.5), area.global_position.y + (size.y * 0.5)
 	)
-	
+
 	return Vector2(rand_x - global_position.x, rand_y - global_position.y)
