b64942c99b059aa935f0a9f9a80847418a3fc58f
Santiago Kent <santiagokent@gmail.com>
2025-02-28 20:50:39 -0300

    feat: gunman now takes time to rotate and aim

--- npc/gunman/gunman.gd
+++ npc/gunman/gunman.gd
@@ -15,7 +15,7 @@ var _fire_timer: float = 0.0
 
 
 static func create() -> Gunman:
-	var instance = Scene.instance()
+	var instance = Scene.instantiate()
 	return instance
 
 
@@ -27,31 +27,27 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	_update_target()
-	_update_fire(delta)
-
-
-func _update_fire(delta: float):
 	if not _target:
-		return
+		return _acquire_target()
 
-	_aim_timer = max(_aim_timer - delta, 0)
-	if _aim_timer > 0:
-		return
+	if not _is_in_sight(_target):
+		return _aim_at(_target, delta)
 
-	_fire_timer = max(_fire_timer - delta, 0)
-	if _fire_timer <= 0:
-		_fire_timer = _fire_time
-		_muzzle_fire_particle.restart()
-		_muzzle_fire_particle.emitting = true
-		_gun_shot_sfx.play()
-		_target.hit.emit(damage)
+	if _is_in_sight(_target):
+		_aim_timer = max(_aim_timer - delta, 0)
+		if _aim_timer > 0:
+			return
+
+		_fire_timer = max(_fire_timer - delta, 0)
+		if _fire_timer <= 0:
+			_fire_timer = _fire_time
+			_muzzle_fire_particle.restart()
+			_muzzle_fire_particle.emitting = true
+			_gun_shot_sfx.play()
+			_target.hit.emit(damage)
 
 
-func _update_target() -> void:
-	if _target:
-		rotation = global_position.angle_to_point(_target.global_position)
-		return
+func _acquire_target() -> void:
 	var enemies = _get_available_targets()
 	if len(enemies):
 		_target = enemies.pick_random()
@@ -59,14 +55,30 @@ func _update_target() -> void:
 		print("[Gunman] Acquired target " + str(_target))
 
 
+func _aim_at(node: Node, delta: float) -> void:
+	var target_rotation = _get_aim_direction(node)
+	self.rotation = rotate_toward(self.rotation, target_rotation, delta * 1.8)
+
+
+func _is_in_sight(node: Node) -> bool:
+	var target_rotation = _get_aim_direction(node)
+	var aiming_angle = lerp_angle(self.rotation, target_rotation, 1)
+	return abs(self.rotation - aiming_angle) < 0.05
+
+
+func _get_aim_direction(node: Node) -> float:
+	return self.global_position.angle_to_point(node.global_position)
+
+
 func _handle_target_died() -> void:
+	print("[Gunman] Target died!")
 	_aim_timer = _rand_aim_time()
 	_fire_timer = _fire_time
 	_target = null
 
 
-func _get_available_targets() -> Array[Enemy]:
-	return (get_tree().get_nodes_in_group("enemies") as Array[Enemy]).filter(_is_in_range)
+func _get_available_targets() -> Array[Node]:
+	return (get_tree().get_nodes_in_group("enemies")).filter(_is_in_range)
 
 
 func _rand_aim_time() -> float:
