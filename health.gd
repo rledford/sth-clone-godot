2fad1576ca84e25037bc95d83708d8bdf7e06d79
Santiago Kent <santiagokent@gmail.com>
2025-02-28 16:43:07 -0300

    feat: create health resource, and move it to the stronghold, add repair upgrade logic

---
+++ health.gd
@@ -0,0 +1,39 @@
+class_name Health
+extends Resource
+
+signal health_changed(health: int, max_health: int)
+signal died
+
+var _max_health: int
+var _health: int
+
+
+func _init(max_health: int) -> void:
+	_max_health = max_health
+	_health = max_health
+
+
+func get_health() -> int:
+	return _health
+
+
+func get_max_health() -> int:
+	return _max_health
+
+
+func take_damage(damage: int) -> void:
+	_change_health(_health - damage)
+
+
+func heal(amount: int) -> void:
+	_change_health(_health + amount)
+
+
+func _change_health(health: int, max_health = _max_health) -> void:
+	_health = clampi(health, 0, max_health)
+	_max_health = max(0, max_health)
+	health_changed.emit(_health, _max_health)
+
+	if _health == 0:
+		died.emit()
+		return
--- stronghold/stronghold.gd
+++ stronghold/stronghold.gd
@@ -1,14 +1,26 @@
-extends Node2D
 class_name Stronghold
+extends Node2D
 
 const Gunman = preload("res://npc/gunman/gunman.tscn")
+
+var _health: Health
 
 
 func _ready() -> void:
 	GunmanUpgrade.new().level_changed.connect(_handle_gunman_upgrade)
+	RepairUpgrade.new().level_changed.connect(_on_repair_level_changed)
+	_health = Health.new(100)
+	_health.health_changed.connect(SignalBus.player_health_changed.emit)
+	_health.died.connect(SignalBus.player_died.emit)
+
+	SignalBus.player_hit.connect(_on_player_hit)
 
 
-func _handle_gunman_upgrade(level: int):
+func get_health() -> Health:
+	return _health
+
+
+func _handle_gunman_upgrade(_level: int):
 	var gunman = Gunman.instantiate()
 	var posts = get_tree().get_nodes_in_group("gunman_posts") as Array[Node2D]
 	var min_occupancy = (
@@ -27,3 +39,11 @@ func _handle_gunman_upgrade(level: int):
 	var target_areas = get_tree().get_nodes_in_group("enemy_spawn_areas") as Array[CollisionShape2D]
 	var area: CollisionShape2D = target_areas.pick_random()
 	gunman.rotation = gunman.global_position.angle_to_point(area.global_position)
+
+
+func _on_player_hit(damage: int) -> void:
+	_health.take_damage(damage)
+
+
+func _on_repair_level_changed(_level: int) -> void:
+	_health.heal(20)
