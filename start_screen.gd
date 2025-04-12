86761cb044620262aae7c3f44c6fe749d1e19fac
Santiago Kent <santiagokent@gmail.com>
2025-04-12 13:18:58 -0300

    feat: add confirmation before wiping the save

--- start_screen.gd
+++ start_screen.gd
@@ -9,6 +9,7 @@ signal start_debug_game_pressed
 const Scene = preload("res://start_screen.tscn")
 
 var _has_saved_game: bool = false
+var _confirm_dialog: ConfirmationDialog
 
 @onready var new_game_btn: Button = %NewGameBtn
 @onready var continue_game_btn: Button = %ContinueGameBtn
@@ -25,9 +26,23 @@ static func create(has_saved_game: bool = false) -> StartScreen:
 func _ready() -> void:
 	new_game_btn.pressed.connect(_on_new_game)
 	continue_game_btn.pressed.connect(_on_continue_game)
-	wipe_save_btn.pressed.connect(_on_wipe_save)
+	wipe_save_btn.pressed.connect(_on_wipe_save_button_pressed)
 	start_debug_game_btn.pressed.connect(_on_start_debug_game)
 	continue_game_btn.visible = _has_saved_game
+
+	_setup_confirm_dialog()
+
+
+func _setup_confirm_dialog() -> void:
+	_confirm_dialog = ConfirmationDialog.new()
+	_confirm_dialog.title = "Confirm Wipe Save Data"
+	_confirm_dialog.dialog_text = """Are you sure you want to wipe all save data?
+	This action cannot be undone."""
+	_confirm_dialog.confirmed.connect(_on_wipe_save_confirmed)
+	_confirm_dialog.get_cancel_button().text = "Cancel"
+	_confirm_dialog.get_ok_button().text = "Wipe Data"
+	_confirm_dialog.min_size = Vector2(300, 100)
+	add_child(_confirm_dialog)
 
 
 func _on_new_game() -> void:
@@ -40,7 +55,11 @@ func _on_continue_game() -> void:
 	queue_free()
 
 
-func _on_wipe_save() -> void:
+func _on_wipe_save_button_pressed() -> void:
+	_confirm_dialog.popup_centered()
+
+
+func _on_wipe_save_confirmed() -> void:
 	wipe_save_pressed.emit()
 
 
